d339a6b08cb89d6992b4ae2cf436fb0603ff3d75
trujilloszitai <trujilloszitai@gmail.com>
2023-11-02 14:19:15 -0300

    post requests rate limiter

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -2,6 +2,8 @@ import { clerkClient } from "@clerk/nextjs";
 import type { User } from "@clerk/nextjs/dist/types/server";
 import { TRPCError } from "@trpc/server";
 import z from "zod";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 
 import {
   createTRPCRouter,
@@ -12,6 +14,13 @@ import {
 const userDataFilter = (user: User) => {
   return { id: user.id, username: user.username, imageUrl: user.imageUrl };
 };
+
+// Allow up to 5 requests per minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(5, "1 m"),
+  analytics: true,
+});
 
 export const postRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
