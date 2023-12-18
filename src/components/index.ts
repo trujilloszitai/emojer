e4cf634cb863f7016707436c572ca52fc03e649d
trujilloszitai <trujilloszitai@gmail.com>
2023-12-18 19:24:37 -0300

    PostView component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Spinner } from "./Spinner";
 export { default as LoadingScreen } from "./LoadingScreen";
+export { default as PostView } from "./PostView";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,11 @@
 import { useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
 import { useUser, SignInButton, UserButton } from "@clerk/nextjs";
 import toast from "react-hot-toast";
-import { Spinner, LoadingScreen } from "~/components";
+import { Spinner, LoadingScreen, PostView } from "~/components";
 
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-
-import { type RouterOutputs, api } from "~/utils/api";
+import { api } from "~/utils/api";
 import { AppLayout } from "~/components/Layout";
 
-dayjs.extend(relativeTime);
 
 export default function Home() {
   const { isLoaded: userLoaded, isSignedIn } = useUser();
@@ -35,39 +29,6 @@ export default function Home() {
     </>
   );
 }
-
-type PostWithUser = RouterOutputs["post"]["getAll"][number];
-const PostView = (props: PostWithUser) => {
-  const { post, author } = props;
-
-  return (
-    <Link href={`/post/${post.id}`}>
-      <div
-        key={post.id}
-        className="flex w-full items-start gap-x-3 border-b border-slate-700 p-8"
-      >
-        <Image
-          src={author.imageUrl}
-          alt={`${author.username}}'s profile image`}
-          width={36}
-          height={36}
-          className="h-10 w-10 rounded-full"
-        />
-        <div className="flex w-full flex-col gap-y-2">
-          <div className="flex w-full items-center justify-between">
-            <Link href={`/@${author.username}`}>
-              <span className="font-medium text-slate-400 hover:text-slate-200">{`@${author.username}`}</span>
-            </Link>
-            <span className="text-sm text-slate-500">
-            • {dayjs(post.createdAt).fromNow()}
-            </span>
-          </div>
-          <span className="text-xl">{post.content}</span>
-        </div>
-      </div>
-    </Link>
-  );
-};
 
 const Feed = () => {
   const { data, isLoading: postsLoading } = api.post.getAll.useQuery();
