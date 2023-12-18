79fa4a008907ba765ed82f9c0e4dce75472a9e20
trujilloszitai <trujilloszitai@gmail.com>
2023-12-18 19:26:44 -0300

    style polishing

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,8 +2,8 @@ import type { PropsWithChildren } from "react";
 
 export const AppLayout = (props: PropsWithChildren) => {
   return (
-    <main className="flex h-screen justify-center overflow-y-scroll">
-      <div className="w-full border-x border-slate-700 md:max-w-2xl">
+    <main className="flex h-screen justify-center">
+      <div className="w-full h-min border-x border-slate-700 md:max-w-2xl">
         {props.children}
       </div>
     </main>
